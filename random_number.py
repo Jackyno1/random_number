d71d4649de63b5ef8cf8d67ee3e12989ab6c1dc7
Jack Chang <a5170159@gmail.com>
2022-04-30 00:43:52 +0800

    add count and range

--- random_number.py
+++ random_number.py
@@ -4,16 +4,27 @@
 #猜對印出"終於猜對了!"
 #猜錯要告訴他 比答案大/小
 
+#印出猜了幾次
+
 import random
 while True:
-	num = random.randint(1,100)
+	lowlimit = input('請輸入最小值:')
+	lowlimit = int(lowlimit)
+	uplimit = input('請輸入最大值:')
+	uplimit = int(uplimit)
+	num = random.randint(lowlimit, uplimit)
+	content = print('請輸入數字',lowlimit, '~', uplimit)
+	i = 0
 	while True:
-		keyin = input('請輸入數字1~100:')
+		keyin = input(content)
 		keyin = int(keyin)
 		if keyin > num:
 			print('比答案大')
+			i = i + 1
 		elif keyin < num:
 			print('比答案小')
+			i = i + 1
 		else:
 			print('終於猜對了!')
+			print('你猜了', i + 1, '次')
 			break
